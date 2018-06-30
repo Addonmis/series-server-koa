c785942baccbd25d8b4f6eae9bdf7512a948464a
addonmis <mulyashov.moris@gmail.com>
2018-06-30 15:01:37 +0300

    fix signup

--- api/modules/auth/controllers/auth.js
+++ api/modules/auth/controllers/auth.js
@@ -1,4 +1,3 @@
-import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 
 import * as tokenServices from "../../../services/tokens";
@@ -40,25 +39,26 @@ export async function signUp(ctx){
     // валидация
     //
 
+    let tokens;
+
     try{
         const salt = await bcrypt.genSalt(10);
         const hash = await bcrypt.hash(password, salt);
 
         await maria.query("insert into users(username, password, id_role, access_token, refresh_token) values(?, ?, 2, ?, ?)", [username, hash, "", ""]);
-        const user = await maria.query("select id_user from users order by id_user desc limit 1;", rows => rows);
-        
-        const tokens = await tokenServices.generateTokens(user[0].id_user);
+        const user = await maria.query("select id_user from users order by id_user desc limit 1;");
+        tokens = await tokenServices.generateTokens(user[0].id_user);
     } catch(e){
         ctx.throw(400, {
             message: e.toString()
         });
     }
-
+    
     ctx.body = { tokens: tokens, id_role: 2 };
 };
 
 export async function getOnTokens(ctx){
     const user = await maria.query("select username, id_role from users where id_user = ?;", [Number(ctx.state.user)]).then((rows) => rows);
-    let tokens = ctx.state.new_tokens;
+    const tokens = ctx.state.new_tokens;
     ctx.body = {id_role: user[0].id_role, tokens: tokens};
 };
